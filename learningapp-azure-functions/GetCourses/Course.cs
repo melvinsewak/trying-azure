61e1775e3e04ccdc3498bc9c6f7f93320c79445d
melvinsewak <melvinsewak@outlook.com>
2024-09-29 13:01:54 -0400

    return course object

---
+++ learningapp-azure-functions/GetCourses/Course.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GetCourses;
+
+public class Course
+{
+    public int CourseID;
+    public string? CourseName;
+    public decimal Rating;    
+
+}
--- learningapp-azure-functions/GetCourses/GetCourses.cs
+++ learningapp-azure-functions/GetCourses/GetCourses.cs
@@ -1,7 +1,10 @@
+using System.Text.Json.Serialization;
+using GetCourses;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace learningapp.azure.function
 {
@@ -18,7 +21,7 @@ namespace learningapp.azure.function
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            return new OkObjectResult(JsonConvert.SerializeObject(new Course(){CourseID=1,CourseName="Dummy",Rating=5}));
         }
     }
 }
