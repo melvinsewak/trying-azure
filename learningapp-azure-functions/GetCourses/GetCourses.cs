07946c5832cd015ec808b4ee06cfff7eb5553257
melvinsewak <melvinsewak@outlook.com>
2024-09-30 21:22:55 -0400

    read config from azure app config

--- learningapp-azure-functions/GetCourses/GetCourses.cs
+++ learningapp-azure-functions/GetCourses/GetCourses.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -15,10 +17,12 @@ namespace learningapp.azure.function
     public class GetCourses
     {
         private readonly ILogger<GetCourses> _logger;
+        private readonly IConfiguration _configuration;
 
-        public GetCourses(ILogger<GetCourses> logger)
+        public GetCourses(ILogger<GetCourses> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [Function("GetCourses")]
@@ -26,7 +30,27 @@ namespace learningapp.azure.function
         {
             _logger.LogInformation("C# HTTP trigger function started processing a request.");
             List<Course> courses = new();
-            courses.Add(new Course(){CourseID=1,CourseName="Dummy",Rating=5});
+            //courses.Add(new Course(){CourseID=1,CourseName="Dummy",Rating=5});
+            
+            var commonSettings = _configuration.GetSection("Common:Settings");
+            var dbPassword = commonSettings.GetValue<string>("dbPassword");
+            string connectionString = string.Format(commonSettings.GetValue<string>("AZURE_SQL_CONNECTIONSTRING")!, dbPassword);
+
+            var sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+
+            var sqlcommand = new SqlCommand(
+            "SELECT CourseID,CourseName,Rating FROM Course;",sqlConnection);
+            using (SqlDataReader sqlDatareader = sqlcommand.ExecuteReader())
+            {
+                while (sqlDatareader.Read())
+                    {
+                        courses.Add(new Course() {CourseID=Int32.Parse(sqlDatareader["CourseID"].ToString()),
+                        CourseName=sqlDatareader["CourseName"].ToString(),
+                        Rating=Decimal.Parse(sqlDatareader["Rating"].ToString())});
+                    }
+            }
+            
             var jsonStr = JsonConvert.SerializeObject(courses);
             _logger.LogInformation("C# HTTP trigger function finished processing a request.");
             return new OkObjectResult(jsonStr);
--- learningapp-azure-functions/GetCourses/Program.cs
+++ learningapp-azure-functions/GetCourses/Program.cs
@@ -1,8 +1,23 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Azure.Identity;
 
 var host = new HostBuilder()
+    .ConfigureAppConfiguration(builder =>
+            {
+                builder
+                .AddEnvironmentVariables()
+                .AddAzureAppConfiguration(options =>{
+                    options.Connect(
+                        new Uri(Environment.GetEnvironmentVariable("AppConfigEndpoint")!),
+                        new ManagedIdentityCredential());
+                    options.UseFeatureFlags();
+                    }
+                );
+            }
+    )
     .ConfigureFunctionsWebApplication()
     .ConfigureServices(services => {
         services.AddApplicationInsightsTelemetryWorkerService();
